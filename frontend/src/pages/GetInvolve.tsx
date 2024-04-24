dafde07a9e84e689bf8a9fe55edf24a4d58db99f
ChanelVintage20 <aorallo43@gmail.com>
2024-04-24 20:52:17 +0800

    getinvolve update

--- frontend/src/pages/GetInvolve.tsx
+++ frontend/src/pages/GetInvolve.tsx
@@ -1,13 +1,55 @@
 import "./Layout.css";
+import "./getInvolve.css"
+import "./Page.css";
+import ACLSFImage from '../images/ACLSF.jpg';
 
 function GetInvolve(){
     return(
     
-        <div  className="main-content">Get Involve
+        <div  className="Home">
+             
+            
+            <img src={ACLSFImage} alt="ACLSF" className="ACLSF" />
+            
+
+           
+
+         <h1>GET INVOLVED!</h1>
+         <h2> Children</h2>
+         <p>Train up a child in the way he should go: and when he is old, he will not depart from it." - Proverbs 22:6 (KJV) Activities: Interactive storytelling, action-songs (clapping, jumping, lifting of hands, and dancing for God), fun games, memory verse, craft projects, many prizes and snacks, and various group activities.
+</p>
+         <br></br> 
+        <hr className="divider"/>
+        <br></br>
+        <p> CLSF has the most outrageous, amazing Vacation Bible School (VBS) where Biblical truths are taught in an amazing high-energy, fun environment.</p>
+       <br></br>
+       <hr className="divider"/>
+       <br></br>
+        <h3> Children are usually divided into three age-groups:</h3>
+        <ul></ul>
+        <li>Toddlers (Ages 3-6)</li>
+        <li>Primaries (Ages 7-9)</li>
+        <li>Juniors (Ages 10 - 12 or 'til 6th Grade)</li>
+        <br></br>
+        <hr className="divider"/>
+
         <div>
-            lalal
+         <h2> Volunteer now!</h2>
+         <li>Children</li>
+         <li>Youth</li>
+         <li>Families</li>
+         <li>Small Groups</li>
+         <li>Media</li>
+         <li>Music and Arts</li>
+         <li>Community</li>   
+ 
+         <br></br>
+         <br></br>
+         <br></br>
+
+
             <div>
-                lallal
+                
             </div>
         </div>
         </div>
